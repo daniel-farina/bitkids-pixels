2689f30c49e1a2d35c8fc4ac64499202361421c4
Daniel Farina <Daniel.Farina@exeloncorp.com>
2023-11-09 20:18:21 -0500

    changing pm2 port, conflicts with other services.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,11 @@ module.exports = {
       watch     : true,
       env: {
         NODE_ENV: 'development',
-        PORT: 3001
+        PORT: 5000
       },
       env_production : {
         NODE_ENV: 'production',
-        PORT: 3001
+        PORT: 5000
       }
     }]
   };
